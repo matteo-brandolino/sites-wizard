76f24e7d1c1f2ac719f606e6f1fc03f98c73b168
matteoB <matteo.brandolino@gmail.com>
2022-09-16 23:51:24 +0200

    remove useless logo

--- src/components/CustomHeader/CustomHeader.tsx
+++ src/components/CustomHeader/CustomHeader.tsx
@@ -9,6 +9,7 @@ import {
   Image,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { MantineLogo } from "@mantine/ds";
 import { HEADER_HEIGHT, useStyles } from "./css";
 
 type Props = {
